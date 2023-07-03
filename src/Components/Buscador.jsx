c028dc580fed3e269858f296520a833c4174c7b0
Pablo Lioy <pablitolioy@hotmail.com>
2023-07-02 23:25:56 -0300

    useState y useEffect

--- src/Components/Buscador.jsx
+++ src/Components/Buscador.jsx
@@ -1,35 +1,37 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { getWeather, getLocation } from "../js/Services"
 
 function Buscador() {
-    const handelData = (lat, long) => {
-        if (latitud && longitud) {
 
+    const [data, setData] = useState(null)
+    const [citys, setcitys] = useState(null)
+    const [latitud, setLatitud] = useState(0)
+    const [longitud, setLongitud] = useState(0)
+
+
+    useEffect(() => {
+        if(!latitud && !longitud) return
             async function query() {
-                const prevData = [];
-                const getquery = await getWeather(latitud, longitud)
-                getquery.hourly.time.map((info, i) => {
-                    return prevData[i] = [info, getquery.hourly.temperature_2m[i]]
-                })
-                setData(prevData)
-            }
-            query()
-            console.log(data.length)
+            const prevData = [];
+            const getquery = await getWeather(latitud, longitud)
+            console.log(getquery)
+            getquery.hourly.time.map((info, i) => {
+                return prevData[i] = [getquery.latitude, getquery.longitude, info, getquery.hourly.temperature_2m[i]]
+                }
+            )
+            setData(getquery)
         }
+        query()
+    }, [latitud, longitud])
 
-        console.log(lat, long)
-        // const lat = document.getElementById("latitud")
-        // const long = document.getElementById("longitud")
 
-    }
+
 
     const handelChange = ({ target }) => {
         async function query() {
             const info = await getLocation(target.value)
             setcitys(info.results)
-            console.log(citys)
         }
-
         query()
     }
 
@@ -37,22 +39,17 @@ function Buscador() {
         e.preventDefault()
         setLatitud(e.target[0].value)
         setLongitud(e.target[1].value)
-        console.log(latitud, longitud)
-        handelData(latitud, longitud)
     }
 
-    const [data, setData] = useState([])
-    const [citys, setcitys] = useState(null)
-    const [latitud, setLatitud] = useState(0)
-    const [longitud, setLongitud] = useState(0)
+
 
     return (
         <>
             <form onSubmit={handelSubmit}>
                 <label htmlFor="latitud">Latitud</label>
-                <input id="latitud" step="0.00001" type="number" defaultValue={latitud} min={-90} max={90} />
+                <input name="latitud" id="latitud" required step="00.01" type="number" min={-90} max={90} />
                 <label htmlFor="longitud">Longitud</label>
-                <input id="longitud" step="0.00001" type="number" defaultValue={longitud} min={-90} max={90} />
+                <input name="longitud" id="longitud" required step="00.01" type="number" min={-90} max={90} />
                 <label htmlFor="city">Ciudad</label>
                 <input id="city" type="text" onChange={handelChange} />
                 {citys &&
@@ -61,14 +58,13 @@ function Buscador() {
                             citys.map((info, i) => {
                                 return (<button
                                     key={i}
-                                    type="submit"
+                                    type="button"
                                     onClick={(e) => {
                                         e.preventDefault()
                                         setLatitud(info.latitude)
                                         setLongitud(info.longitude)
-                                        console.log(latitud, longitud)
-                                        handelData(latitud, longitud)
-                                        }
+                                        setcitys([])
+                                    }
                                     }
                                 >{info.name + " " + info.admin1 + " " + info.latitude + " " + info.longitude}</button>)
                             })
@@ -78,11 +74,8 @@ function Buscador() {
 
                 <button type="submit">Buscar</button>
             </form>
-            {data &&
-                data.map((info, i) => {
-                    return (<h3 key={i}>{info[0] + " " + info[1] + "°"}</h3>)
-                })
-            }
+            {data}
+            
         </>
     )
 }
